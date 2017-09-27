bc102ae0a98e679de30511c7327bd50be34b01c6
JherSter <jeroenavis123@gmail.com>
2017-09-27 11:52:48 +0200

    Push1

---
+++ ProefOpdracht_DialogSystem/Assets/DialogManager.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour
+{
+	private XMLparser _xml;
+	[SerializeField]private Text _question, _name, _answers;
+	private Dropdown _dropdown;
+	private bool _answered;
+	
+	
+	
+	
+
+
+	private void Start()
+	{
+		_xml = GameObject.FindGameObjectWithTag("XmlParser").GetComponent<XMLparser>();
+		_dropdown = GetComponentInChildren<Dropdown>();
+		
+
+
+	}
+
+	private void Update()
+	{
+		switch (_dropdown.value)
+		{
+				
+					case 0:
+						if (!_answered)
+						{
+							NpcDailogue(1,0);
+							
+						}
+						
+						break;
+					case 1:
+						
+						if (!_answered)
+						{
+							NpcDailogue(0,1);
+							
+						}
+						
+						break;
+				
+			default:
+				break;
+		}
+
+
+		if (!Input.GetKeyDown(KeyCode.Alpha1)) return;
+		_answered = true;
+		Yesanswer();
+		
+		if (!Input.GetKeyDown(KeyCode.Alpha2)) return;
+		_answered = true;
+		NoAnswer();
+	}
+
+	private void Yesanswer()
+	{
+		_answers.text = _xml.Answers;
+		
+	}
+	private void NoAnswer()
+	{
+		_answers.text = _xml.Farwell;
+		
+	}
+
+	private void NpcDailogue(int value1, int value2)
+	{
+		
+		_xml.PageNume = value1;
+		_xml.Interation = value2;
+		_name.text = _xml.NameOfNpc;
+		_question.text = _xml.Dialog;
+		print(_answered);
+	}
+
+	
+}
---
+++ ProefOpdracht_DialogSystem/Assets/XMLparser.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+using System.Collections.Generic;
+
+using System.Xml; 
+
+using System.Xml.Serialization;
+
+using System.IO;
+
+using System.Xml.Linq;
+
+
+
+public class XMLparser : MonoBehaviour
+{
+	public string NameOfNpc, Dialog, Answers, Farwell;
+	public int Interation = 0, PageNume = 0;
+	private XDocument _xmlDoc;
+	private IEnumerable<XElement> _items;
+
+
+	private readonly List <XmlData> _data = new List <XmlData>();
+	
+		
+
+	// Use this for initialization
+	private void Start () {
+		DontDestroyOnLoad(gameObject);
+		LoadXml();
+		StartCoroutine(Assign());
+	}
+
+	private void LoadXml()
+	{
+		_xmlDoc = XDocument.Load("Assets/conversation.xml");
+		_items = _xmlDoc.Descendants("page").Elements();
+	}
+	private void Update(){
+		StartCoroutine(Assign());
+	}
+
+	private IEnumerator Assign()
+	{
+		
+		foreach (var item in _items)
+		{
+			if (item.Parent.Attribute("number").Value != Interation.ToString()) continue;
+			PageNume = int.Parse(item.Parent.Attribute("number").Value);
+			NameOfNpc = item.Parent.Element("name").Value.Trim ();
+			Dialog = item.Parent.Element("dialogue").Value.Trim();
+			Answers = item.Parent.Element("answers").Value.Trim();
+			Farwell = item.Parent.Element("farwell").Value.Trim();
+			_data.Add(new XmlData(PageNume, NameOfNpc, Dialog));
+
+			
+		}
+		yield return null;
+	}
+	public class XmlData
+	{
+		public int PageNum;
+		public string nameOfNPC;
+		public string DialogueText;
+
+		public XmlData(int page, string character, string dialog)
+		{
+			PageNum = page;
+			nameOfNPC = character;
+			DialogueText = dialog;
+		}
+	}
+	
+	
+}
+
